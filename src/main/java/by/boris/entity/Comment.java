e545935fddadc61cda2ff50fd9033cfa1894e6ed
Boris Mazepin <borikmazepin@mail.ru>
2020-12-10 14:34:57 +0300

    add the style of the main page (css)

--- src/main/java/by/boris/entity/Comment.java
+++ src/main/java/by/boris/entity/Comment.java
@@ -2,10 +2,7 @@ package by.boris.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -13,6 +10,7 @@ public class Comment {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
+    @Column(length = 5_000)
     private String text;
     private String country;
 
